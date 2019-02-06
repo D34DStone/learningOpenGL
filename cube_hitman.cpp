3595978d3a90c96fe6cd241c2424e53d50f972ec
D34DStone <deadstonepro@gmail.com>
2019-02-06 22:13:29 +0200

    I begin to understating how it works

---
+++ cube_hitman.cpp
@@ -0,0 +1,183 @@
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+class Shader
+{
+private:
+    unsigned int ID;
+
+    bool checkIfCompiled(GLuint shader, const std::string &type)
+    {
+        GLint success;
+        GLchar infoLog[1024];
+        if(type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if(!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cerr << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << std::endl;
+                return false;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if(!success)
+            {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cerr << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << std::endl;
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    Shader(const std::string &vertexShaderFile, const std::string &fragmentShaderFile)
+    {
+        std::fstream inFile;
+        std::string sourceCode;
+
+        inFile.open(vertexShaderFile);
+        if(!inFile.is_open())
+        {
+            std::cerr << "Unable to open " << vertexShaderFile << std::endl;
+            return;
+        }
+        sourceCode = std::string(
+            std::istreambuf_iterator<char>(inFile),
+            std::istreambuf_iterator<char>()
+        );
+        inFile.close();
+
+        unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        const char *vertexShaderCode = sourceCode.c_str();
+        glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
+        glCompileShader(vertexShader);
+        if(!checkIfCompiled(vertexShader, "VERTEX"))
+        {
+            return;
+        }
+
+        inFile.open(fragmentShaderFile);
+        if(!inFile.is_open())
+        {
+            std::cerr << "Unable to open " << fragmentShaderFile << std::endl;
+            return;
+        }
+        sourceCode = std::string(
+            std::istreambuf_iterator<char>(inFile),
+            std::istreambuf_iterator<char>()
+        );
+
+        unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        const char *fragmentShaderCode = sourceCode.c_str();
+        glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
+        glCompileShader(fragmentShader);
+        if(!checkIfCompiled(fragmentShader, "FRAGMENT"))
+        {
+            return;
+        }
+
+        this->ID = glCreateProgram();
+        glAttachShader(this->ID, fragmentShader);
+        glAttachShader(this->ID, vertexShader);
+        glLinkProgram(this->ID);
+        checkIfCompiled(this->ID, "PROGRAM");
+
+        glDeleteShader(fragmentShader);
+        glDeleteShader(vertexShader);
+    }
+
+    void use()
+    {
+        glUseProgram(this->ID);
+    }
+};
+
+void initGL(void)
+{
+    glViewport(0, 0,  WINDOW_WIDTH, WINDOW_HEIGHT);
+    glEnable(GL_DEPTH_TEST);
+}
+
+void onResize(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    initGL();
+    glfwSetFramebufferSizeCallback(window, onResize);
+
+    Shader shader("cube_hitman_ver.glsl", "cube_hitman_frag.glsl");
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };  
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    return 0;
+};
--- hitman_polygon.cpp
+++ hitman_polygon.cpp
@@ -1,22 +1,32 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <string>
+#include <fstream>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec4 ourColor;\n"
+    "uniform mat4 transform;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+    "   gl_Position = transform * vec4(aPos, 1.0);\n"
+    "   ourColor = vec4(aColor, 1.0);\n"
+    "}\n\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
+    "in vec4 ourColor;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = ourColor;\n"
     "}\n\0";
 
 void initGL(void)
@@ -36,6 +46,13 @@ void processInput(GLFWwindow *window)
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
+class Shader
+{
+public:
+    Shader(const std::string &vertexShader, const std::string &shader);
+private:
+};
 
 int main()
 {
@@ -66,13 +83,6 @@ int main()
 
     // вешаем калбэк функцию на ресайз окна
     glfwSetFramebufferSizeCallback(window, onResize);
-
-    float verticies[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f, 
-        0.0f, 0.5f, 0.0f
-    };
-
     unsigned int vertexShader, fragmentShader;
     int success;
     char infoLog[512];
@@ -108,24 +118,53 @@ int main()
         std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
 
-    // Загружаем данные в видеопамять. Создаем VBO
-    unsigned int VBO, VAO;
-    glGenBuffers(1, &VBO);
+    float vertices[] = {
+         0.5f,  0.5f, 0.5f,   1, 0, 0,
+         0.5f, -0.5f, 0.5f,   0, 1, 0,
+        -0.5f, -0.5f, 0.5f,   0, 0, 1,
+        -0.5f,  0.5f, 0.5f,   1, 1, 0,
 
+        0.5f,  0.5f, -0.5f,   1, 0, 1,
+        0.5f, -0.5f, -0.5f,   0, 1, 1,
+        -0.5f, -0.5f, -0.5f,  0, 0, 0,
+        -0.5f,  0.5f, -0.5f,  1, 1, 1
+    };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3,
+        3, 2, 7, 
+        7, 2, 6, 
+        7, 6, 4, 
+        6, 4, 5, 
+        4, 5, 1,
+        4, 0, 1,
+        3, 7, 0, 
+        7, 0, 4,
+        2, 6, 1, 
+        1, 6, 5
+    };
+
+    // Загружаем данные в видеопамять. Создаем VBO
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-    // указываем как в нашей памяти размещаются вершины:
-    // первый аргумент: индекс
-    // второй аргумент: количество чисел в одной вершине
-    // третий аргумент: тип значений
-    // четверый аргумент: суммарный размер одной вершины
-    // пятый аргумент: какое-то говно непонятное. Зачем оно вообще тут?
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // как-то разберусь. Вроде эта штука выставляет начало нумерации аргументов. А мб и нет :3
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glEnable(GL_DEPTH_TEST);
 
     // основной цикл
     while(!glfwWindowShouldClose(window))
@@ -133,9 +172,16 @@ int main()
         // проверяем на нажатия, движения мыши этсэтра...
         processInput(window);
 
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 transform = glm::mat4(1.0f);
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(1.0f, 0.75f, 0.5f));
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        glBindVertexArray(1);
+        glDrawElements(GL_TRIANGLES, 6 * 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
